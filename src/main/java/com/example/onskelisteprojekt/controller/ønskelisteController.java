4f104df41bd48af6a59bc5f63c8063a514e288f6
TobiasSolv <tobiasurban@hotmail.com>
2022-10-25 11:13:06 +0200

    der er lavet noget

---
+++ src/main/java/com/example/onskelisteprojekt/controller/ønskelisteController.java
@@ -0,0 +1,19 @@
+package com.example.onskelisteprojekt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ønskelisteController {
+
+    @GetMapping("/homepage")
+    public String homepage(){
+        return "homepage";
+    }
+
+    @GetMapping("/wishlist")
+    public String wishlist(){
+        return "wishlist";
+    }
+
+}
