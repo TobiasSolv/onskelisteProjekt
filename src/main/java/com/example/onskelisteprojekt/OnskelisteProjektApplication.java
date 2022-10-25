9f93f30c2c005d64c65af99c1066fa58c0c8f123
TobiasSolv <tobiasurban@hotmail.com>
2022-10-25 09:55:31 +0200

    Initial commit

---
+++ src/main/java/com/example/onskelisteprojekt/OnskelisteProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.onskelisteprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskelisteProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnskelisteProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onskelisteprojekt/OnskelisteProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onskelisteprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskelisteProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
